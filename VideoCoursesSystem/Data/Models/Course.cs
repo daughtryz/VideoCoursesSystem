697b98b030c5b65cf59f1eebbc446d220cc2470c
daughtryz <dimitar.iliev4321@abv.bg>
2021-05-08 23:49:41 +0300

    FixBug

--- VideoCoursesSystem/Data/Models/Course.cs
+++ VideoCoursesSystem/Data/Models/Course.cs
@@ -15,26 +15,26 @@ namespace VideoCoursesSystem.Data.Models
         }
         public string Id { get; set; }
 
-        [Required]
+        //[Required]
         public DateTime StartDate { get; set; }
-        [Required]
+        //[Required]
         public DateTime EndDate { get; set; }
 
         public DateTime Time { get; set; }
         public string EventContext { get; set; }
         
-        [Required]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        //[Required]
+        //[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         public string Title { get; set; }
 
-        [Required]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        //[Required]
+        //[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         public string Component { get; set; }
 
         public string EventName { get; set; }
         public PlatformType PlatformType { get; set; }
 
-        [Required]
+       // [Required]
         public string Description { get; set; }
         public int Viewers { get; set; }
         public ICollection<Exercise> Exercises { get; set; }
