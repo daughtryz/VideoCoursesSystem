683be26b3f880b0109ed9860d9d4ba177998ce7c
daughtryz <dimitar.iliev4321@abv.bg>
2021-05-30 18:04:01 +0300

    Unit tests added

---
+++ VideoCourseSystem.UnitTests/CoursesServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VideoCourseSystem.UnitTests
+{
+    public class CoursesServiceTests
+    {
+        private LogsInformationService _logInformationService;
+        public CoursesServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "VideoCoursesSystem");
+            _logInformationService = new LogsInformationService(new ApplicationDbContext(options.Options));
+        }
+        [Fact]
+        public void ItShouldUploadCourse()
+        {
+
+        }
+    }
+}
---
+++ VideoCourseSystem.UnitTests/LogInformationServiceTests.cs
@@ -0,0 +1,36 @@
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoCoursesSystem.Data;
+using VideoCoursesSystem.Data.Models;
+using VideoCoursesSystem.Services;
+using Xunit;
+
+namespace VideoCourseSystem.UnitTests
+{
+    public class LogInformationServiceTests
+    {
+        private LogsInformationService _logInformationService;
+        public LogInformationServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "VideoCoursesSystem");
+            _logInformationService = new LogsInformationService(new ApplicationDbContext(options.Options));
+        }
+        [Fact]
+        public async Task ItShouldAccessTheLogs()
+        {
+            var appUser = new ApplicationUser
+            {
+                Id = "123",
+                UserName = "Test"
+            };
+            var log = await _logInformationService.CreateLogAsync($"The user with id '{appUser.Id}' viewed the course with id '111'.");
+            await _logInformationService.CreateUserLogAsync(log.Id, appUser.Id);
+            string expectedDescription = "The user with id '123' viewed the course with id '111'.";
+            var currentLog = _logInformationService.GetAllLogs().FirstOrDefault(l => l.Id == log.Id);
+            Assert.Equal(expectedDescription, currentLog.Description);
+        }
+    }
+}
